fd507cf430d949513976978df7f21a78a7fdefeb
eunlee <eklee2057@naver.com>
2025-04-19 22:07:25 +0900

    폭탄 해제 주석

--- 12-graph/ex01.cpp
+++ 12-graph/ex01.cpp
@@ -16,6 +16,7 @@ int main(void) {
 
 	std::cin >> N >> M;
 
+	// 그래프와 간선 정보를 저장
 	for (int i = 0; i < M; ++i) {
 		std::cin >> v1 >> v2;
 		graph[v1].push_back(v2);
@@ -23,12 +24,14 @@ int main(void) {
 		edges.push_back({v1, v2});
 	}
 
+	// 간선을 순회하면서, 이 연결을 자를 수 없는 경우를 저장
 	for (int i = 0; i < M; ++i) {
 		if (graph[edges[i].first].size() == 1 || graph[edges[i].second].size() == 1)
-			continue;
+			continue ;
 		res.push_back(i + 1);
 	}
 
+	// 저장된 간선의 번호를 출력
 	if (!res.size()) std::cout << "-1";
 	else
 		for (size_t i = 0; i < res.size(); ++i) std::cout << res[i] << " ";
