c4aefa9de04d537b76e21ddb370bf13a512a9518
eunlee <eklee2057@naver.com>
2025-05-16 13:57:32 +0900

    빵야

--- 05-simulation/ex03.cpp
+++ 05-simulation/ex03.cpp
@@ -20,13 +20,15 @@ int main() {
 		// 권총의 대미지는 1, 2, 3, 4, 1, 2, 3, 4, ... 를 반복
 		// 4번의 발사 1, 2, 3, 4가 한 세트
 
-		// ememy의 체력을 권총 4회(한 바퀴)를 발사한 결과인 damage로 나누기
+		// ememy의 체력을 권총 4회(한 바퀴)를 발사한 결과인 10으로 나누기
 		count += enemies[i] / 10 * 4;
+
+		// 나머지를 취하기
 		enemies[i] %= 10;
 
-		// 나머지 체력에서 발사 반복, modulo로 현재 포인터 저장
+		// 나머지 체력에 대해 발사 반복, modulo로 현재 pointer 저장
 		while (enemies[i] > 0) {
-			enemies[i] -= (pointer++ + 1);
+			enemies[i] -= (pointer++ + 1); // pointer 값의 구간은 1, 2, 3, 4를 순환
 			pointer %= 4;
 			++count;
 		}
